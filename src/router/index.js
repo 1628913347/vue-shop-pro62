fe71fe92e5c95cbddac84fe20a81524ec431d240
liguojin <1628913347@qq.com>
2019-02-15 10:17:05 +0800

    项目文件完成

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/home', component: Home },
+    { path: '/login', component: Login }
+  ]
+})
